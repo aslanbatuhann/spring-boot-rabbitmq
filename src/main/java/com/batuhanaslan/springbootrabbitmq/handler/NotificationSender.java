d616466cd05085e8ecab01b1b3f5b5b2ff0851a0
Batuhan Aslan <batuhanaslan9601@gmail.com>
2022-10-04 08:22:50 +0300

    listener producer sender

---
+++ src/main/java/com/batuhanaslan/springbootrabbitmq/handler/NotificationSender.java
@@ -0,0 +1,43 @@
+package com.batuhanaslan.springbootrabbitmq.handler;
+
+import com.batuhanaslan.springbootrabbitmq.model.Notification;
+import com.batuhanaslan.springbootrabbitmq.producer.NotificationProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class NotificationSender {
+
+    @Autowired
+    private NotificationProducer producer;
+
+    public void getThread() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                Notification notification = new Notification();
+                notification.setNotificationId(UUID.randomUUID().toString());
+                notification.setCreatedAt(new Date());
+                notification.setMessage("Batuhandan Mesaj Var...");
+                notification.setSeen(false);
+                producer.sendToQueue(notification);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.setName("Notification sender");
+        thread.start();
+    }
+
+    @PostConstruct
+    public void init() {
+        getThread();
+
+    }
+}
--- src/main/java/com/batuhanaslan/springbootrabbitmq/listener/NotificationListener.java
+++ src/main/java/com/batuhanaslan/springbootrabbitmq/listener/NotificationListener.java
@@ -1,11 +1,15 @@
 package com.batuhanaslan.springbootrabbitmq.listener;
 
 import com.batuhanaslan.springbootrabbitmq.model.Notification;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NotificationListener {
 
+    @RabbitListener(queues = "batuhan-aslan-queue")
     public void handleMessage(Notification notification){
-
+        System.out.println("Message received");
         System.out.println(notification.toString());
     }
 }
